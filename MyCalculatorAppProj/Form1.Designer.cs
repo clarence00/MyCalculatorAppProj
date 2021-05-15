4e79de11a44171dc03986ba53ed6db19133a1cfa
clarence00 <clarencenatividad12@gmail.com>
2021-05-15 12:23:44 +0800

    Operations added

--- MyCalculatorAppProj/Form1.Designer.cs
+++ MyCalculatorAppProj/Form1.Designer.cs
@@ -30,24 +30,84 @@ namespace MyCalculatorAppProj
         private void InitializeComponent()
         {
             this.Display = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDivi = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Display
             // 
             this.Display.BackColor = System.Drawing.Color.Silver;
-            this.Display.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Display.Location = new System.Drawing.Point(8, 9);
+            this.Display.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Display.Location = new System.Drawing.Point(4, 6);
             this.Display.Name = "Display";
-            this.Display.Size = new System.Drawing.Size(336, 58);
+            this.Display.Size = new System.Drawing.Size(305, 70);
             this.Display.TabIndex = 0;
             this.Display.Text = "0";
             this.Display.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(4, 78);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(153, 51);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "CE";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDivi
+            // 
+            this.btnDivi.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDivi.Location = new System.Drawing.Point(234, 78);
+            this.btnDivi.Name = "btnDivi";
+            this.btnDivi.Size = new System.Drawing.Size(75, 51);
+            this.btnDivi.TabIndex = 2;
+            this.btnDivi.Text = "/";
+            this.btnDivi.UseVisualStyleBackColor = true;
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMulti.Location = new System.Drawing.Point(158, 78);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(75, 51);
+            this.btnMulti.TabIndex = 3;
+            this.btnMulti.Text = "X";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSub.Location = new System.Drawing.Point(234, 130);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 51);
+            this.btnSub.TabIndex = 4;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(234, 182);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 51);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 412);
+            this.ClientSize = new System.Drawing.Size(313, 347);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnDivi);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.Display);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,6 +118,11 @@ namespace MyCalculatorAppProj
         #endregion
 
         private System.Windows.Forms.Label Display;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDivi;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
